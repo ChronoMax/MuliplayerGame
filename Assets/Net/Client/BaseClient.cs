0a3a4b31f48f7d408b3a25746d2662b016146cf0
Max Struijs <maxstruys32@gmail.com>
2023-03-16 20:26:17 +0100

    Client multiplayer code

---
+++ Assets/Net/Client/BaseClient.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using Unity.Collections;
+using Unity.Networking.Transport;
+
+public class BaseClient : MonoBehaviour
+{
+    public NetworkDriver driver; //the interface to speak anything network related.
+    protected NetworkConnection connection;
+
+#if UNITY_EDITOR //Only works in the unity editor! (This is to make the application run outside the editor.
+    private void Start() { Init(); }
+    private void Update() { UpdateServer(); }
+    private void OnDestroy() { Shutdown(); }
+#endif
+
+    /*Virtual is used because this is a 'base' class.
+     * If you want to override something you can do that in a child class.
+     */
+    public virtual void Init()
+    {
+        //Init the driver.
+        driver = NetworkDriver.Create(); //Creating the driver
+        connection = default(NetworkConnection); //setting the connection to default.
+
+        NetworkEndPoint endPoint = NetworkEndPoint.LoopbackIpv4; //connect to localhost
+        //NetworkEndPoint endPoint = NetworkEndPoint.TryParse("192.168.178.122"); //connect to other ip
+        endPoint.Port = 5522;
+
+        connection = driver.Connect(endPoint); //connection to server
+    }
+    public virtual void Shutdown()
+    {
+        //Deletes the connection.
+        driver.Dispose();
+    }
+    public virtual void UpdateServer()
+    {
+        driver.ScheduleUpdate().Complete();
+        CheckAlive();
+        UpdateMessagePump();
+    }    
+    private void CheckAlive()
+    {
+        if (!connection.IsCreated)
+        {
+            Debug.Log("Lost connection to server!");
+        }
+    }
+    protected virtual void UpdateMessagePump() //Server pharse all message that clients send to server to apply to server.
+    {
+        DataStreamReader stream;
+
+        NetworkEvent.Type cmd;
+        while ((cmd = connection.PopEvent(driver, out stream)) != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Connect)
+            {
+                Debug.Log("You are connected to the server.");
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                uint value = stream.ReadByte();
+                Debug.Log("Got value from the server: " + value);
+            }
+            else if (cmd == NetworkEvent.Type.Disconnect)
+            {
+                Debug.Log("Disconnected from the server!");
+            }
+        }
+    }
+}
